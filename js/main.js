1c58b852a7ce87ced1ef4a1dbc430bd14b962b98
Zhiwei Yin <zhiweiyin@hotmail.com>
2019-01-29 00:24:00 +0800

    modify format

--- js/main.js
+++ js/main.js
@@ -180,3 +180,35 @@ var main = {
 // 2fc73a3a967e97599c9763d05e564189
 
 document.addEventListener('DOMContentLoaded', main.init);
+
+/**
+ * Table of Content fix
+ */
+function initToc() {
+  const $toc = $('#post-toc');
+  if ($toc.length && $(window)
+    .width() >= 1080) {
+    $(window)
+      .scroll(function() {
+        if ($(window)
+          .scrollTop() > 100) {
+          $toc.fadeIn(1000);
+        } else {
+          $toc.fadeOut(100);
+        }
+      });
+  }
+}
+
+var toc = function() {
+  const tocContainer = document.getElementById('post-toc');
+  if (tocContainer !== null) {
+    const toc = document.getElementById('TableOfContents');
+    if (toc === null) {
+      // if global config 'toc = true', but there are no headings
+      tocContainer.parentNode.removeChild(tocContainer);
+    } else {
+      initToc();
+    }
+  }
+}
